52c4e3e8744adbc34f006628ac3c16cb530d9e01
emmanuelAkpe <emmanuelakpe1@gmail.com>
2022-10-18 22:00:24 +0000

    defined the trying method class and the main method

---
+++ TryingMethods.java
@@ -0,0 +1,15 @@
+public class TryingMethods {
+    /*
+     * The trying methods has a number of methods 
+     * that we use to perform specific tasks
+     */
+  
+
+     public static void main(String[] args) {
+        
+
+        
+     }
+
+
+}
