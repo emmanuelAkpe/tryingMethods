888bd8d522af66b663436586014444907efccc9f
emmanuelAkpe <emmanuelakpe1@gmail.com>
2022-10-18 22:03:57 +0000

    dynamic method 1--day of week

--- TryingMethods.java
+++ TryingMethods.java
@@ -3,10 +3,39 @@ public class TryingMethods {
      * The trying methods has a number of methods 
      * that we use to perform specific tasks
      */
-  
+  String dayOfWeek(int day){
+        switch(day){
+            case 1:
+                return "Monday";
+            case 2:
+                return "Tuesday";
+            case 3:
+                return "Wednesday";
+            case 4:
+                return "Thursday";
+            case 5:
+                return "Friday";
+            case 6:
+                return "Saturday";
+            case 7:
+                return "Sunday";
+            default:
+                return "Invalid Day";
+        }
+    }
+
+   
 
      public static void main(String[] args) {
         
+               // Creating an Instance of the class
+        TryingMethods dynamTryingMethods = new TryingMethods();
+      
+            System.out.println("Dynamic Methods");
+        String day = dynamTryingMethods.dayOfWeek(4);
+         System.out.println("The day is "+ day);
+
+
 
         
      }
